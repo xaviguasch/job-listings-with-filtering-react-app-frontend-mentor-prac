f30d87c46329d50f08c9edc6f59a659c49a47d3b
xaviguasch <xaviguasch@gmail.com>
2022-01-19 18:59:49 +0100

    added JobCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
 
+import JobCard from './components/JobCard'
+
 function App() {
   return (
     <div className='App'>
       <h1>Job Listings App</h1>
+
+      <JobCard />
     </div>
   )
 }
---
+++ src/components/JobCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './JobCard.css'
+
+const JobCard = () => {
+  return (
+    <div className='JobCard'>
+      <h2>JobCard</h2>
+    </div>
+  )
+}
+
+export default JobCard
